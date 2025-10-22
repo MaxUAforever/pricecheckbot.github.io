9b8e2d71a7566def3cc45498ca6d980f804523ec
MaxUAforever <maxuaforever@gmail.com>
2025-10-22 16:40:40 +0200

    Modify base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/pricecheckbot.github.io/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
